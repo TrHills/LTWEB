6054a59f564a9ffaf5cfc18ff59a14fcbea37522
TrHills <tatrunghieucm@gmail.com>
2024-10-07 19:06:58 +0700

    first

---
+++ 23DH111065_TaTrungHieu_KtraFE(TH)/23DH111065_TaTrungHieu_KtraFE(TH)/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _23DH111065_TaTrungHieu_KtraFE_TH_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ 23DH111065_TaTrungHieu_KtraFE(TH)/23DH111065_TaTrungHieu_KtraFE(TH)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23DH111065_TaTrungHieu_KtraFE_TH_.MvcApplication" Language="C#" %>
